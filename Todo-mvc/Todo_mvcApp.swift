3b5ef99f635cfbe6752ccfdb9b81175e8a383e0e
Tahmid Akter <tahmidakter56@gmail.com>
2023-09-09 11:04:42 +0600

    Initial Commit

---
+++ Todo-mvc/Todo_mvcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_mvcApp.swift
+//  Todo-mvc
+//
+//  Created by Tahmid Akter on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_mvcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
